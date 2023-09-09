01c46b9a5e45a461b10bc7d2fe8ab20a6c82758c
Jukelyn <viperkillergaming@gmail.com>
2023-09-09 01:53:25 -0400

    pog

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 # Import necessary libraries and modules
-from my_model import MyModel  # Replace with your actual language model
+from transformers import BertTokenizer, BertForSequenceClassification
+
+model_name = "bert-base-uncased"
+tokenizer = BertTokenizer.from_pretrained(model_name)
+
 from sklearn.preprocessing import LabelEncoder
 
+# The inputs to be categorized
 entries = []
 
 def read_entry():
     # read entry and add it to entries list
+    pass
 
 def main():
     # read input submission somehow maybe with read_entry() ?
 
     # Define the categories
+    '''
+    categories = ["Health and Wellness", "Family", "Achievements", "Environment",
+              "Material Possessions", "Community", "Miscellaneous"]
+    '''
     categories = ["Family", "Self Love", "Career", "Social"]
 
     # Create an instance of MyModel (replace with your actual model)
-    model = MyModel()
+    model = BertForSequenceClassification.from_pretrained(model_name, num_labels=len(categories))
 
     # Classify the entries into categories
     predictions = model.classify(entries)
