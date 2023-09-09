95087f8fc6ebe4eed95a61e7b0ba128471e398ee
Jukelyn <viperkillergaming@gmail.com>
2023-09-09 04:05:40 -0400

    pls work

--- docs/inputQuery.js
+++ docs/inputQuery.js
@@ -1,6 +1,1 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const fd = new FormData(form);
-    console.log(fd);
-});
+// tf
