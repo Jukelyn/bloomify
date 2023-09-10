20ac3a74f31200eb2ba35e545f8f309691123eb4
Jukelyn <viperkillergaming@gmail.com>
2023-09-10 16:11:23 -0400

    Two Girls Kissing

--- docs/inputQuery.js
+++ docs/inputQuery.js
@@ -1,27 +1,37 @@
-const form = document.getElementById('formm');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getStorage, ref, uploadString } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js";
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    // apiKey: "${process.env.API_KEY}",
+    apiKey: "AIzaSyDTjZc234GAMzUgXlk5gTISANWyaOhT7Jc",
+    authDomain: "jukelyn-s-site.firebaseapp.com",
+    databaseURL: "https://jukelyn-s-site-default-rtdb.firebaseio.com",
+    projectId: "jukelyn-s-site",
+    storageBucket: "jukelyn-s-site.appspot.com",
+    messagingSenderId: "980268158913",
+    // appId: "${process.env.APP_ID}",
+    appId: "1:980268158913:web:717628d0946324733ae940",
+    measurementId: "G-9P75C95WCP"
+};
+
+const app = initializeApp(firebaseConfig);
+// Create a root reference
+const storage = getStorage();
+const storageRef = ref(storage, 'user_inputs');
+
+// const form = document.getElementById('formm');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault(); // Prevent the form from submitting the traditional way
 
+    const answer = 'This is my message.'; // this should be the user input
+    uploadString(storageRef, message).then((snapshot) => {
+        console.log('Uploaded a raw string!');
+    });
     const ans = document.getElementById('ans').value;
 
-    // Call a function to save the data to Firestore
-    saveDataToFirestore(ans);
+    // Call a function to save the data to Storage
+    saveInput(ans);
 });
-
-function saveDataToFirestore(ans) {
-    const db = app.firestore();
-
-    db.collection('answers').add({
-        answer: ans,
-    })
-
-        .then((docRef) => {
-            console.log('Document written with ID: ', docRef.id);
-            // Optionally, clear the form after successful submission
-            form.reset();
-        })
-        .catch((error) => {
-            console.error('Error adding document: ', error);
-        });
-}
