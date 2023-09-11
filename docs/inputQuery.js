311ce9f12570d013f3440b966ca02df50934e357
Jukelyn <viperkillergaming@gmail.com>
2023-09-10 20:27:47 -0400

    The Smile at the End of the Dock

--- docs/inputQuery.js
+++ docs/inputQuery.js
@@ -18,53 +18,10 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig);
 const storage = getStorage();
-const storageRef = ref(storage, "user_input.txt"); // Define the filename
 
 // Get references to DOM elements
 const inputForm = document.getElementById("input-form");
 const textInput = document.getElementById("text-input");
-
-// Function to read the existing content of the file (if it exists)
-function readExistingContent() {
-    getDownloadURL(storageRef)
-        .then((url) => {
-            const xhr = new XMLHttpRequest();
-            xhr.responseType = "text";
-            xhr.onload = function () {
-                if (xhr.status === 200) {
-                    const existingContent = xhr.responseText;
-                    // Append the new text input to the existing content
-                    const newText = textInput.value;
-                    const updatedContent = existingContent + "\n" + newText;
-                    // Upload the updated content
-                    uploadString(storageRef, updatedContent)
-                        .then((snapshot) => {
-                            console.log("Text appended and uploaded successfully!");
-                        })
-                        .catch((error) => {
-                            console.error(`Error uploading text: ${error.message}`);
-                        });
-                } else {
-                    // Handle errors when reading the existing content
-                    console.error("Error reading existing content.");
-                }
-            };
-
-            xhr.open("GET", url);
-            xhr.send();
-        })
-        .catch((error) => {
-            // If the file doesn't exist, create it with the new text input
-            const newText = textInput.value;
-            uploadString(storageRef, newText)
-                .then((snapshot) => {
-                    console.log("Text uploaded successfully!");
-                })
-                .catch((error) => {
-                    console.error("Error uploading text: ${error.message}");
-                });
-        });
-}
 
 // Add an event listener to the form for when it's submitted
 inputForm.addEventListener("submit", function (e) {
@@ -73,11 +30,22 @@ inputForm.addEventListener("submit", function (e) {
     const text = textInput.value; // Get the text from the input field
 
     if (text.trim() !== "") {
-        // Read existing content and then append/upload the new text
-        readExistingContent();
+        // Define a file name for the text content (e.g., user_input.txt)
+        const fileName = `user_input_${Date.now()}.txt`;
+
+        // Create a reference to the file in Firebase Storage
+        const storageRef = ref(storage, fileName);
+
+        // Upload the text content as a string
+        uploadString(storageRef, text).then((snapshot) => {
+            console.log("Text uploaded successfully!");
+        }).catch((error) => {
+            console.error("Error uploading text: ${error.message}");
+        });
+
         // Clear the input field
         textInput.value = "";
     } else {
-        console.error("Please enter some text to append/upload.");
+        console.error("Please enter some text to upload.");
     }
 });
